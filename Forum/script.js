d70959c007ff4a57e45b30e155f396fcbc2b6714
nzl-nassim <120056539+nzl-nassim@users.noreply.github.com>
2024-01-26 11:11:16 +0100

    Add files via upload

---
+++ Forum/script.js
@@ -0,0 +1,14 @@
+function test() {
+
+    open("https://fr.w3docs.com/apprendre-html/mise-en-forme-html.html");
+}
+
+function test2() {
+
+    open("https://fr.w3docs.com/apprendre-html/mise-en-forme-html.html");
+}
+
+function test3() {
+
+    open("https://fr.w3docs.com/apprendre-html/mise-en-forme-html.html");
+}
